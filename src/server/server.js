79d6e18a80b166a766f31522921787a22b48d4a6
jyoon006 <jyoon006@gmail.com>
2016-08-17 10:45:06 -0400

    add react-native files

---
+++ src/server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var parser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.get('/', function(req, res) {
+  res.json('server deployed and is running');
+});
+
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function() {
+  console.log('Server listening on port ' + PORT);
+});
+
+module.exports = app;
