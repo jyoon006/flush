e3eed71ccf290de236f93e5690805dbb36a0fb05
jyoon006 <jyoon006@gmail.com>
2016-08-17 13:22:06 -0400

    render simple map

--- src/client/components/MapComponent.js
+++ src/client/components/MapComponent.js
@@ -5,11 +5,48 @@ import {
   View
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 class MapComponent extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialPosition: {
+      }
+    };
+  }
+
+  componentDidMount() {
+    let context = this;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log('position', position);
+      var location = {
+        latitude: position.coords.latitude, 
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.022,
+        longitudeDelta: 0.022,
+      };
+      context.setState({initialPosition: location});
+
+    }, function(err) {
+      console.log('error getting current geolocation', err);
+    },
+    { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+  }
+
   render() {
     return(
-      <View style={styles.container}>
-        <Text>THIS IS MAP COMPONENT</Text>
+      <View>
+        <MapView
+          style={styles.map}
+          region={this.state.initialPosition} >
+          <MapView.Marker
+            coordinate={this.state.initialPosition}
+            title='testing marker'
+            description='testing testing'
+          />
+        </MapView>
       </View>
     )
   }
@@ -22,6 +59,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 400,
   }
 });
 
