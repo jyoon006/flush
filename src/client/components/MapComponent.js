88c70768b1faa0f03e9e97e5336e9ed49a1c9a64
jyoon006 <jyoon006@gmail.com>
2016-08-18 22:56:30 -0400

    add viewport units functionality to viewport.js added simple button to add places in map component

--- src/client/components/MapComponent.js
+++ src/client/components/MapComponent.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 import MapView from 'react-native-maps';
+import {vw, vh, vmin, vmax} from '../viewport.js';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class MapComponent extends Component {
   
@@ -31,8 +34,14 @@ class MapComponent extends Component {
             coordinate={this.state.initialPosition}
             title='testing marker'
             description='testing testing'
+            image={require('../assets/pin.png')}
           />
         </MapView>
+         
+            <TouchableOpacity style={styles.addButton}>
+              <Icon name="plus-circle" size={60} color="#009ACD"/>
+            </TouchableOpacity>  
+       
       </View>
     )
   }
@@ -46,13 +55,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  addButton:{
+    width: 20 * vw,
+    color:'#fff',
+    textAlign:'center',
+    top: 90 * vh,
+    left: 80 * vw
+  },
   map: {
     position: 'absolute',
-    top: 0,
+    top: 10 * vh,
     left: 0,
     right: 0,
     bottom: 0,
-    height: 400,
+    height: 90* vh,
   }
 });
 
